cd7d992cf69c476b2421c36ce7ed07ed04dbfb50
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-04-01 14:57:24 -0300

    VentaService terminado

--- src/main/java/com/coderhouse/proyectofinal/repository/VentaRepository.java
+++ src/main/java/com/coderhouse/proyectofinal/repository/VentaRepository.java
@@ -4,6 +4,9 @@ import com.coderhouse.proyectofinal.model.transactions.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VentaRepository extends JpaRepository<Venta,Integer> {
+    List<Venta> findAllByCuil(int cuil);
 }
