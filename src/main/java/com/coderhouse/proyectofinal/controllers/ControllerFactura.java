e5d608034720ccf1b316cbb6c4c65d03dfe5e5c6
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-04-01 15:45:57 -0300

    ControllerFactura transformado a @RestController

--- src/main/java/com/coderhouse/proyectofinal/controllers/ControllerFactura.java
+++ src/main/java/com/coderhouse/proyectofinal/controllers/ControllerFactura.java
@@ -7,29 +7,36 @@ import com.coderhouse.proyectofinal.model.ticket.FacturaB;
 import com.coderhouse.proyectofinal.model.ticket.FacturaC;
 import com.coderhouse.proyectofinal.model.user.Carrito;
 import com.coderhouse.proyectofinal.model.user.Client;
+import com.coderhouse.proyectofinal.service.ticket.FacturaAService;
+import com.coderhouse.proyectofinal.service.ticket.FacturaBService;
+import com.coderhouse.proyectofinal.service.ticket.FacturaCService;
 import org.springframework.cglib.core.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@RestController
+@RequestMapping("/facturas")
 public class ControllerFactura {
 
-    private int idFactura;
-    private List<FacturaA> listadoDeFacturaA;
-
-    private List<FacturaB> listadoDeFacturaB;
-
-    private List<FacturaC> listadoDeFacturaC;
+    //Declarando los servicios que voy a necesitar
+    private FacturaAService facturaAService;
+    private FacturaBService facturaBService;
+    private FacturaCService facturaCService;
 
     private static ControllerFactura instancia;
 
     //Constructor
     public ControllerFactura(){
-        this.listadoDeFacturaA = new ArrayList<>();
-        this.listadoDeFacturaB = new ArrayList<>();
-        this.listadoDeFacturaC = new ArrayList<>();
+
     }
 
     //Metodo getInstancia para que sea singleton
@@ -42,96 +49,84 @@ public class ControllerFactura {
     }
 
     //Metodos de la clase
-
-    public void generarFacturaA(int nroFactura, int cuilCliente, LocalDate fecha, int idCarrito)
-            throws UserNotFoundException, FacturaNotFoundException {
-        if (buscarFacturaA(nroFactura) == null){
-            ControllerUser controllerUser = ControllerUser.getInstancia();
-            Client cliente = controllerUser.getClient(cuilCliente);
-
-            Carrito carrito = cliente.getCarrito();
-
-            FacturaA facturaA = new FacturaA(nroFactura,
-                    cliente.getCuil(), LocalDate.of(fecha.getYear(),fecha.getMonthValue(),fecha.getDayOfMonth())
-                    ,carrito.getTotal());
-
-            idFactura ++;
-
-            listadoDeFacturaA.add(facturaA);
-        }
+    @PostMapping(value = "/agregarFacturaA", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<FacturaA> guardarFacturaA(@RequestBody FacturaA facturaA){
+        facturaAService.guardarFacturaA(facturaA);
+        return new ResponseEntity<>(facturaA, HttpStatus.OK);
     }
 
-    public void generarFacturaB(int nroFactura, int cuilCliente,LocalDate fecha,int idCarrito)
-            throws UserNotFoundException, FacturaNotFoundException {
-        if (buscarFacturaB(nroFactura) == null){
-            ControllerUser controllerUser = ControllerUser.getInstancia();
-            Client cliente = controllerUser.getClient(cuilCliente);
-
-            Carrito carrito = cliente.getCarrito();
-
-            FacturaB facturaB = new FacturaB(nroFactura,
-                    cliente.getCuil(), LocalDate.of(fecha.getYear(),fecha.getMonthValue(),fecha.getDayOfMonth())
-                    ,carrito.getTotal());
-
-            idFactura ++;
-
-            listadoDeFacturaB.add(facturaB);
-        }
+    @PostMapping(value = "/agregarFacturaB", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<FacturaB>guardarFacturaB(@RequestBody FacturaB facturaB){
+        facturaBService.guardarFacuraB(facturaB);
+        return new ResponseEntity<>(facturaB,HttpStatus.OK);
     }
-    public void generarFacturaC(int nroFactura, int cuilCliente,LocalDate fecha,int idCarrito)
-            throws UserNotFoundException, FacturaNotFoundException{
-        if (buscarFacturaC(nroFactura) == null){
-            ControllerUser controllerUser = ControllerUser.getInstancia();
-            Client cliente = controllerUser.getClient(cuilCliente);
 
-            Carrito carrito = cliente.getCarrito();
-
-            FacturaC facturaC = new FacturaC(nroFactura,
-                    cliente.getCuil(), LocalDate.of(fecha.getYear(),fecha.getMonthValue(),fecha.getDayOfMonth())
-                    ,carrito.getTotal());
-
-            idFactura ++;
-
-            listadoDeFacturaC.add(facturaC);
-        }
+    @PostMapping(value = "/agregarFacturaC", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<FacturaC>guardarFacturaC(@RequestBody FacturaC facturaC){
+        facturaCService.guardarFacutraC(facturaC);
+        return new ResponseEntity<>(facturaC,HttpStatus.OK);
     }
 
 
-    //Metodo privado que devuelve un objeto de negocio que el cliente nunca debe ver
-    private FacturaA buscarFacturaA(int nroFactura)
-        throws FacturaNotFoundException {
-        for (FacturaA fA :
-                listadoDeFacturaA) {
-            if (nroFactura == fA.getNroFactura()) {
-                return fA;
-            }
+    @GetMapping(value = "/facturaA", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaA>>obtnenerFacturasA(){
+        try {
+            List<FacturaA> listadoDeFacturaA = facturaAService.listarFacturasA();
+            return new ResponseEntity<>(listadoDeFacturaA,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        throw new FacturaNotFoundException("No hay una factura de tipo A " +
-                "creada con el numero: " + nroFactura);
     }
 
-    private FacturaB buscarFacturaB(int nroFactura)
-            throws FacturaNotFoundException {
-        for (FacturaB fB :
-                listadoDeFacturaB) {
-            if (nroFactura == fB.getNroFactura()) {
-                return fB;
-            }
+    @GetMapping(value = "/facturaB", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaB>>obtenerFacturasB(){
+        try {
+            List<FacturaB> listadoDeFacturaB = facturaBService.listarFacturasB();
+            return new ResponseEntity<>(listadoDeFacturaB,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        throw new FacturaNotFoundException("No hay una factura de tipo B " +
-                "creada con el numero: " + nroFactura);
     }
 
-    private FacturaC buscarFacturaC(int nroFactura)
-            throws FacturaNotFoundException {
-        for (FacturaC fC :
-                listadoDeFacturaC) {
-            if (nroFactura == fC.getNroFactura()) {
-                return fC;
-            }
+    @GetMapping(value = "/facturaC", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaC>>obtenerFacturasC(){
+        try {
+            List<FacturaC> listadoDeFacturaC = facturaCService.listarFacturasC();
+            return new ResponseEntity<>(listadoDeFacturaC,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        throw new FacturaNotFoundException("No hay una factura de tipo C " +
-                "creada con el numero: " + nroFactura);
     }
+
+    @GetMapping(value = "/facturaA/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaA>>obtenerFacturasADeUnCliente(@PathVariable("id") Integer cuil){
+        try {
+            List<FacturaA>facturasCliente = facturaAService.listarFacturasADeUnCliente(cuil);
+            return new ResponseEntity<>(facturasCliente,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/facturaB/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaB>>obtenerFacturasBDeUnCliente(@PathVariable("id") Integer cuil){
+        try {
+            List<FacturaB>facturascliente = facturaBService.listarFacturasBDeUnCliente(cuil);
+            return new ResponseEntity<>(facturascliente,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/facturaC/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<FacturaC>>obtenerFacutrasCDeUnCliente(@PathVariable("id") Integer cuil){
+        try {
+            List<FacturaC>facturasCliente = facturaCService.listarFacturasCDeUnCliente(cuil);
+            return new ResponseEntity<>(facturasCliente,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
