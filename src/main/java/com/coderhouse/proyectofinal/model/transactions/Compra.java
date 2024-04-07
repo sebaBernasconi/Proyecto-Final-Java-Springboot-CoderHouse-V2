efd8829ca87e935e15608ad4dbc3809bf89c5f67
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-04-07 13:01:11 -0300

    Cambios en las anotaciones de compra

--- src/main/java/com/coderhouse/proyectofinal/model/transactions/Compra.java
+++ src/main/java/com/coderhouse/proyectofinal/model/transactions/Compra.java
@@ -5,12 +5,16 @@ import com.coderhouse.proyectofinal.model.user.Admin;
 import com.coderhouse.proyectofinal.model.payment.MedioDePago;
 import com.coderhouse.proyectofinal.model.user.Carrito;
 import com.coderhouse.proyectofinal.model.user.Client;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.util.Calendar;
 import java.util.Date;
 
+@Entity
+@Table(name = "compra")
 public class Compra extends Transaccion {
     @ManyToOne
     @JoinColumn(name = "cuil_cliente")
@@ -23,10 +27,15 @@ public class Compra extends Transaccion {
     //Constructor
 
     public Compra(Date fecha, Carrito carrito,
-                   float total, Client client,Admin vendedor) {
-        super( fecha,carrito, total);
+                  float total, Client client, Admin vendedor) {
+        super(fecha, carrito, total);
         this.client = client;
         this.vendedor = vendedor;
+    }
+
+    //Constructor vacio para la persistencia
+    public Compra() {
+
     }
 
     //Metodos de la clase
