00d6e1e0abe2a4d36690c790dbbc68268886ca15
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-04-07 13:16:16 -0300

    Anotaciones agregadas a la clase Admin

--- src/main/java/com/coderhouse/proyectofinal/model/user/Admin.java
+++ src/main/java/com/coderhouse/proyectofinal/model/user/Admin.java
@@ -1,11 +1,18 @@
 package com.coderhouse.proyectofinal.model.user;
 
 import com.coderhouse.proyectofinal.model.transactions.Venta;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.util.List;
 
+@Entity
+@Table(name = "administradores")
 public class Admin extends User {
 
+    @OneToMany(mappedBy = "cuil_admin", cascade = CascadeType.ALL)
     private List<Venta> ventas;
 
     //Constructor
@@ -14,6 +21,11 @@ public class Admin extends User {
                  String mail, String password, List<Venta> ventas) {
         super(cuil, nombre, mail, password);
         this.ventas = ventas;
+    }
+
+    //Constructor vacio para la persistencia
+    public Admin() {
+
     }
 
     //Getters y Setters
