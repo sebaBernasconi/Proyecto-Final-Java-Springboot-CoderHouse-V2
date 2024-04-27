8ea907c8405d7b5351fdd16df566159e60582378
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-04-27 18:07:22 -0300

    Configuracion de Swagger aparentemente lista

--- src/main/java/com/coderhouse/proyectofinal/config/OpenApiConfig.java
+++ src/main/java/com/coderhouse/proyectofinal/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.coderhouse.proyectofinal.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Proyecto Final | Curso Java y Springboot")
+                        .version("1.0.0")
                         .description("Proyecto sobre un e-comerce de venta de comics y figuras de accion" +
                                 "CRUD(CREATE, READ, UPDATE, DELETE) para los clientes, admins, productos, " +
                                 "transacciones, medios de pago. Todo esto puede ser accedido mediante los endpoints" +
@@ -20,6 +22,10 @@ public class OpenApiConfig {
                                 "El lenguaje utilizado fue Java con el framework de Springboot para la conexion a la " +
                                 "base de datos y la conformacion de la API REST Full." +
                                 "Swagger para la documentacion de la api.")
+                        .contact(new Contact()
+                                .name("Sebastian Bernasconi")
+                                .email("bernaseba1@gmail.com")
+                                .url("https://github.com/sebaBernasconi/Proyecto-Final-Java-Springboot-CoderHouse-V2"))
                 );
     }
 }
