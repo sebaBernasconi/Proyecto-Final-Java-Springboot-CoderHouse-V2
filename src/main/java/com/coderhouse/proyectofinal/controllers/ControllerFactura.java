57211d511f30725fc14f66678210f0af9dff4ea3
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-01 12:35:24 -0300

    Anotaciones de Swagger terminadas en el ControllerFactura

--- src/main/java/com/coderhouse/proyectofinal/controllers/ControllerFactura.java
+++ src/main/java/com/coderhouse/proyectofinal/controllers/ControllerFactura.java
@@ -2,6 +2,7 @@ package com.coderhouse.proyectofinal.controllers;
 
 import com.coderhouse.proyectofinal.exception.FacturaNotFoundException;
 import com.coderhouse.proyectofinal.exception.UserNotFoundException;
+import com.coderhouse.proyectofinal.model.ticket.Factura;
 import com.coderhouse.proyectofinal.model.ticket.FacturaA;
 import com.coderhouse.proyectofinal.model.ticket.FacturaB;
 import com.coderhouse.proyectofinal.model.ticket.FacturaC;
@@ -10,6 +11,12 @@ import com.coderhouse.proyectofinal.model.user.Client;
 import com.coderhouse.proyectofinal.service.ticket.FacturaAService;
 import com.coderhouse.proyectofinal.service.ticket.FacturaBService;
 import com.coderhouse.proyectofinal.service.ticket.FacturaCService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
@@ -26,6 +33,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/facturas")
+@Tag(name = "Gestion de Facutras",description = "Endpoints de las Facturas")
 public class ControllerFactura {
 
     //Declarando los servicios que voy a necesitar
@@ -56,18 +64,45 @@ public class ControllerFactura {
     }
 
     //Metodos de la clase
+    @Operation(summary = "Agregar Factura A")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Factura A guardada en la base de datos",
+            content = {
+                    @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+            }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+
+    })
     @PostMapping(value = "/agregarFacturaA", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<FacturaA> guardarFacturaA(@RequestBody FacturaA facturaA){
         facturaAService.guardarFacturaA(facturaA);
         return new ResponseEntity<>(facturaA, HttpStatus.OK);
     }
 
+    @Operation(summary = "Agregar Factura B")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Factura B guardada en la base de datos",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+                    }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+
+    })
     @PostMapping(value = "/agregarFacturaB", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<FacturaB>guardarFacturaB(@RequestBody FacturaB facturaB){
         facturaBService.guardarFacuraB(facturaB);
         return new ResponseEntity<>(facturaB,HttpStatus.OK);
     }
 
+    @Operation(summary = "Agregar Factura C")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Factura C guardada en la base de datos",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+                    }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+
+    })
     @PostMapping(value = "/agregarFacturaC", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<FacturaC>guardarFacturaC(@RequestBody FacturaC facturaC){
         facturaCService.guardarFacutraC(facturaC);
@@ -75,6 +110,14 @@ public class ControllerFactura {
     }
 
 
+    @Operation(summary = "Listar Factura A")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Devuelve un listado de todas las FacturaA hechas",
+            content = {
+                    @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+            }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+    })
     @GetMapping(value = "/facturaA", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<FacturaA>>obtnenerFacturasA(){
         try {
@@ -85,6 +128,14 @@ public class ControllerFactura {
         }
     }
 
+    @Operation(summary = "Listar Factura B")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Devuelve un listado de todas las FacturaB hechas",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+                    }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+    })
     @GetMapping(value = "/facturaB", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<FacturaB>>obtenerFacturasB(){
         try {
@@ -95,6 +146,14 @@ public class ControllerFactura {
         }
     }
 
+    @Operation(summary = "Listar Factura C")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Devuelve un listado de todas las FacturaC hechas",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Factura.class))
+                    }),
+            @ApiResponse(responseCode = "500",description = "Error interno del servidor", content = @Content)
+    })
     @GetMapping(value = "/facturaC", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<FacturaC>>obtenerFacturasC(){
         try {
