e438bd8cf802d91cf2a06742879edd4b4415c862
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-02 11:52:24 -0300

    Anotaciones de Swagger terminadas en el modelo Venta

--- src/main/java/com/coderhouse/proyectofinal/model/transactions/Venta.java
+++ src/main/java/com/coderhouse/proyectofinal/model/transactions/Venta.java
@@ -6,18 +6,22 @@ import com.coderhouse.proyectofinal.model.user.Admin;
 import com.coderhouse.proyectofinal.model.user.Carrito;
 import com.coderhouse.proyectofinal.model.user.Client;
 import com.coderhouse.proyectofinal.model.payment.MedioDePago;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 import java.util.Date;
 
+@Schema(description = "Modelo de Venta")
 @Entity
 @Table(name = "venta")
 public class Venta extends Transaccion {
 
+    @Schema(description = "Cliente asociado a la Venta", requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne
     @JoinColumn(name = "cuil_cliente")
     private Client client;
 
+    @Schema(description = "Admin asociado a la Venta",requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne
     @JoinColumn(name = "cuil_admin")
     private Admin vendedor;
