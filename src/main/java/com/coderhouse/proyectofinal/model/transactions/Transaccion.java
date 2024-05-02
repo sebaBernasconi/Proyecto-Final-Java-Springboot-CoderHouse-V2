b03317980a69f621c49c7a8da2453d81bc2af683
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-02 11:47:44 -0300

    Anotaciones de Swagger terminadas en el modelo Transaccion

--- src/main/java/com/coderhouse/proyectofinal/model/transactions/Transaccion.java
+++ src/main/java/com/coderhouse/proyectofinal/model/transactions/Transaccion.java
@@ -4,28 +4,37 @@ import com.coderhouse.proyectofinal.model.payment.Debito;
 import com.coderhouse.proyectofinal.model.ticket.Factura;
 import com.coderhouse.proyectofinal.model.user.Carrito;
 import com.coderhouse.proyectofinal.model.payment.MedioDePago;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 import java.util.Date;
 
+@Schema(description = "Modelo de Transaccion")
 @Entity
 public abstract class Transaccion {
 
+    @Schema(description = "Id autogenerado de la transaccion")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int idTransaccion;
 
+    @Schema(description = "Carrito asociado a la transaccion",requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne
     @JoinColumn(name = "id_carrito")
     private Carrito carrito;
 
+    @Schema(description = "Fecha de la Transaccion",requiredMode = Schema.RequiredMode.REQUIRED,
+    example = "2024-05-30")
     @Column(name = "fecha")
     private Date fecha;
 
+    @Schema(description = "Total de la Transaccion",requiredMode = Schema.RequiredMode.REQUIRED,
+    example = "234.53")
     @Column(name = "total")
     private float total;
 
+    @Schema(description = "Factura asociada a la Transaccion(si la tiene)", requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne
     @JoinColumn(name = "nro_factura")
     private Factura factura;
